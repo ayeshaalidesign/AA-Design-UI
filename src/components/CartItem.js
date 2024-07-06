8746d8995f2bb2953078d9e92125c861eb1d9c26
Hamad Ali <hamad@metricapp.co>
2024-07-07 00:50:00 +0500

    fix: image click in cart to redirect back to product page

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -15,7 +15,7 @@ const CartItem = ({ item }) => {
     <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
       <div className="w-full min-h-[150px] flex items-center gap-x-4">
   
-        <Link to={`/product/${ProductPrice}`}>
+        <Link to={`/products/${ProductId}`}>
           <img className="max-w-[80px]" src={ImageUrls[0]} alt="Product" />
         </Link>
         <div className="w-full flex flex-col">
