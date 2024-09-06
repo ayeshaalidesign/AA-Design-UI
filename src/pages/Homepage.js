3ff395835b18b4ea9d3223acbc8e7633feff5fa8
Hamad Ali <136328582+hamad-ali123@users.noreply.github.com>
2024-09-07 01:58:15 +0500

    fix: homepage pic

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BootstrapCarousel from "../components/Carousel";
-import ready from "../images/cover.jpeg";
-import cover from '../images/Coverr.jpg';
+import occasionWear from '../images/Cover.jpeg';
+import shawl from '../images/Coverr.jpg'
 import "../App";
 
 const HomePage = () => {
@@ -9,20 +9,38 @@ const HomePage = () => {
   const handleRedirection = () => {
     window.location.href = '/products';
   }
+
+  const handleRedirectionShawl = () => {
+    window.location.href = '/shawls';
+  }
+
   return (
     <div className="font-verdana">
       <BootstrapCarousel />
-      
-      <img src={cover} alt="coming soon" className="w-full" />
       <div className="relative">
-        <img src={ready} alt="ready to wear" className="w-full" />
+        <img src={shawl} alt="Embroidered Shawl" className="w-full" />
         {/* <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 text-white font-semibold font-verdana text-xs mb-[-25px] lg:text-lg lg:ml-[-40px]">
           Ready to Wear
         </div> */}
-        <div className="absolute bottom-12 left-1/2 transform -translate-x-1/2 text-white font-thin italic font-verdana text-xs mb-[-12px] lg:text-lg lg:ml-[-40px]">
-          Occasion Wear
+        <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2 text-white font-bold italic font-verdana text-xs mb-[-12px] lg:text-lg lg:ml-[-40px]">
+        Embroidered Shawl
         </div>
-        <div className="absolute bottom-3 left-1/2 transform -translate-x-1/2 text-white font-semibold font-verdana cursor-pointer text-xs lg:text-xl lg:ml-[-40px] underline" onClick={handleRedirection}>
+        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-white font-semibold font-verdana cursor-pointer text-xs lg:text-xl lg:ml-[-40px] underline" onClick={handleRedirectionShawl}>
+        <p>
+        Shop
+        </p>
+        </div>
+      </div>
+
+      <div className="relative">
+        <img src={occasionWear} alt="Occasion Wear" className="w-full" />
+        {/* <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 text-white font-semibold font-verdana text-xs mb-[-25px] lg:text-lg lg:ml-[-40px]">
+          Ready to Wear
+        </div> */}
+        <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2 text-white font-bold italic font-verdana text-xs mb-[-12px] lg:text-lg lg:ml-[-40px]">
+        Occasion Wear
+        </div>
+        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 text-white font-semibold font-verdana cursor-pointer text-xs lg:text-xl lg:ml-[-40px] underline" onClick={handleRedirection}>
         <p>
         Shop
         </p>
