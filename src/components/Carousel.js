b4502251c04e85265457fdda2595a81fb3dcd260
Hamad Ali <136328582+hamad-ali123@users.noreply.github.com>
2024-10-18 23:29:18 +0500

    fix: deployment issue

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import HomePageVideo from '../video/homepage-video.mp4';
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import HomepageVideo from '../video/homepage.mp4';
+
 const BootstrapCarousel = () => {
-
   return (
-<div className="carousel-container">
-  <Carousel controls={false} indicators={false}>
-    <Carousel.Item>
-      <div className="relative">
-        <video autoPlay muted loop playsInline className="w-full">
-          <source src={HomePageVideo} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    </Carousel.Item>
-  </Carousel>
-</div>
-
+    <div className="carousel-container">
+      <Carousel controls={false} indicators={false}>
+        <Carousel.Item>
+          <div className="relative">
+            <video autoPlay muted loop playsInline className="w-full">
+              <source src={HomepageVideo} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 };
 
